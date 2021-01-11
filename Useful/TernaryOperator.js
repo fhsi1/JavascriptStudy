d0c2798d504f5a8007eec3b64ced8979b9b4530d
fhsi1 <eugenie8@kakao.com>
2021-01-12 03:42:05 +0900

    Useful_TernaryOperator

---
+++ Useful/TernaryOperator.js
@@ -0,0 +1,21 @@
+// Ternary Operator.js
+
+// Example
+// const array = [];
+// let text = '';
+// if (array.length === 0) {
+// 	text = 'Empty';
+// } else {
+// 	text = 'Not Empty';
+// }
+// console.log(text);
+
+const array = [];
+let text = array.length === 0 ? 'Empty' : 'Not Empty';
+console.log(text);
+
+// const array = [];
+// let text = array.length === 0
+// 	? 'Empty'
+// 	: 'Not Empty';
+// console.log(text);
